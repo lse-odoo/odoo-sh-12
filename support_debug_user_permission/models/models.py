50d4a007eb62fdd6956b8a1320fb1fef795168b8
Loan (lse) <lse@odoo.com>
2020-03-04 13:30:57 +0100

    Added group debuging files

---
+++ support_debug_user_permission/models/models.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import traceback
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+class User(models.Model):
+    _name = 'res.users'
+    _inherit = ['res.users']
+
+    @api.multi
+    def write(self, values):
+        before_user_perm_set = set(self.groups_id.ids)
+
+        res = super().write(values)
+
+        after_user_perm_set = set(self.groups_id.ids)
+
+        if before_user_perm_set != after_user_perm_set:
+            intersection = after_user_perm_set & before_user_perm_set
+            new_permission = after_user_perm_set - intersection
+            removed_permission = before_user_perm_set - intersection
+
+            _logger.info(
+                "\n*SUPPORT* Groups permissions changed.\nModified user: %s\nUID: %s \nNew: %s \nRemoved: %s \nStacktrace:\n %s\n",
+                self.name, self.env.user.name, new_permission, removed_permission, ''.join(traceback.format_stack()))
+
+        return res
+
+class Group(models.Model):
+    _name = 'res.groups'
+    _inherit = ['res.groups']
+
+    @api.multi
+    def write(self, values):
+        before_users = set(self.users.ids)
+
+        res = super().write(values)
+
+        after_users_set = set(self.users.ids)
+        if before_users != after_users_set:
+            intersection = after_users_set & before_users
+            new_permission = after_users_set - intersection
+            removed_permission = before_users - intersection
+
+            _logger.info(
+                "\n*SUPPORT* Groups permissions changed.\nModified user: %s\nUID: %s \nNew: %s \nRemoved: %s \nStacktrace:\n %s\n",
+                self.name, self.env.user.name, new_permission, removed_permission, ''.join(traceback.format_stack()))
+
+        return res
