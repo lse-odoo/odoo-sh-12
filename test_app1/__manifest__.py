79e34595655e4fa3aacb2ba0ee03d063fe9e6f0a
Loan (lse) <lse@odoo.com>
2021-05-28 15:55:38 +0200

    short change

--- test_app1/__manifest__.py
+++ test_app1/__manifest__.py
@@ -4,7 +4,7 @@
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        subtitle on modules listing or apps.openerp.com. Short change""",
 
     'description': """
         Long description of module's purpose
